28c4398e82e87bd925f1a8bf439d678efa63ddde
bgood92 <goodfellow.brandon@gmail.com>
2018-03-13 14:56:26 -0500

    Added validations to VideoGame and Genre.

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -1,2 +1,5 @@
 class VideoGame < ApplicationRecord
+  validates :name, :price, :stock_quantity, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_quantity, numericality: { only_integer: true, greater_than: 0 }
 end

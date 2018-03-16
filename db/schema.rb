ef35bd6bbc95dbe00d205b5aea3b335f7f792f44
bgood92 <goodfellow.brandon@gmail.com>
2018-03-16 14:15:11 -0500

    Added an Address column to Customer model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316190314) do
+ActiveRecord::Schema.define(version: 20180316191259) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20180316190314) do
     t.integer "province_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["province_id"], name: "index_customers_on_province_id"
   end
 

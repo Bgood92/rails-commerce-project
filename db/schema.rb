2c16deebfc5836fc292ee10ec0b22cd0630420f3
bgood92 <goodfellow.brandon@gmail.com>
2018-03-16 16:04:57 -0500

    Added pST and HST columns to Province

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316210225) do
+ActiveRecord::Schema.define(version: 20180316210405) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -112,6 +112,8 @@ ActiveRecord::Schema.define(version: 20180316210225) do
     t.decimal "gst"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "pst"
+    t.decimal "hst"
   end
 
   create_table "statuses", force: :cascade do |t|

0d1bed775948140dea12ba20151e946b2e7aa600
bgood92 <goodfellow.brandon@gmail.com>
2018-03-16 13:50:33 -0500

    Added validations to Country, Customer, and Province.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   belongs_to :province
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates_format_of :email, with: /\A.+@.+\Z/i
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,3 +1,5 @@
 class Province < ApplicationRecord
   belongs_to :country
+  validates :name, :tax_rate, presence: true
+  validates :tax_rate, numericality: { greater_than_or_equal_to: 0 }
 end

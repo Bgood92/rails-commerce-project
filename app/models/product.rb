9497599f949785f4de4fcdffe0062acc271dfbbe
bgood92 <goodfellow.brandon@gmail.com>
2018-03-16 14:00:43 -0500

    Created a join model called ProductGenre model that joins Product and Genre to resolve many-to-many association.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  # has many genres
+  has_many :order_histories
+  has_many :orders, through: :order_histories
   validates :name, :price, :stock_quantity, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :stock_quantity, numericality: { only_integer: true, greater_than: 0 }

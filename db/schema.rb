7d12ba9d1bba3d17576d08565e38791242b6147d
bgood92 <goodfellow.brandon@gmail.com>
2018-03-29 16:59:49 -0500

    Added sale_price column to Product.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329215628) do
+ActiveRecord::Schema.define(version: 20180329215811) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20180329215628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "on_sale"
+    t.decimal "sale_price"
   end
 
   create_table "provinces", force: :cascade do |t|
